934159eab25db906a36db268bf70ba1c8334d256
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-07-19 01:13:40 -0500

    last update

--- concepts.js
+++ concepts.js
@@ -1,16 +1,38 @@
 
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import React from "react";
 
-const middleware = [thunk];
+const UserAdminTable = ({ users, onEdit, onDelete }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>ExpireDate</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.expiredate}</td>
+            <td>{user.role}</td>
+            <td>
+              <button onClick={() => onEdit(user.id)}>Edit</button>
+              <button onClick={() => onDelete(user.id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
-export default store;
+export default UserAdminTable;
 
             
