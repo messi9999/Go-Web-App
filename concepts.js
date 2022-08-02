ceb4c25c62bd34832586bf32466adbb182b51857
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-08-02 01:16:32 -0500

    responsive

--- concepts.js
+++ concepts.js
@@ -1,173 +1,118 @@
 
-import "./HomeScreen.css";
 import React from "react";
-import Header from "../components/Header";
-
-import newsImg1 from "../img/news1.png";
-import newsImg2 from "../img/news2.png";
-import newsImg3 from "../img/news3.png";
-import { ReactComponent as LogoIcon } from "../img/icon.svg";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
-export default function HomeScreen() {
-  const navigate = useNavigate();
+import { Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../components/Header";
+import { Button, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASEURL;
+export default function ProfileScreen() {
   const { user: currentUser } = useSelector((state) => state.auth);
+
+  const handleOnClick = async () => {
+    axios
+      .put(`${BASE_URL}/payment/cancel/${currentUser.id}`, {
+        SUBSCRIPTION_ID: currentUser.subscriptionId
+      })
+      .then((res) => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        window.location.reload();
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
   return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
-      <div style={{ height: "12vh" }}>
+    <div style={{ height: "18vh" }}>
+      <div className="home-main bg-black mb-0 bg-gradient py-3">
         <Header />
       </div>
-      <div className="main-body text-white">
-        <div className="main-body-text">
-          <div>
-            Let's Generate <br />
+      <div className="bg-black" style={{ height: "110vh" }}>
+        <div className="text-white text-center d-flex flex-column justify-content-center align-content-center w-100 pt-5">
+          <div className="pb-5">
+            <h1>{currentUser.username}</h1>
           </div>
           <div>
-            <span className="text-success">Newsletters</span> with AI!
+            <h4>Email: {currentUser.email}</h4>
           </div>
-          <div className="d-flex justify-content-center align-items-center w-100">
-            <div>
-              {currentUser &&
-              (currentUser.subscriptionStatus === "active" ||
-                currentUser.subscriptionStatus === "trialing") ? (
-                <button
-                  className="btn btn-md btn-success rounded-5"
-                  style={{}}
-                  onClick={() => {
-                    navigate("/mainscreen");
-                  }}
-                >
-                  CREATE NEWSLETTER
-                </button>
-              ) : (
-                <button
-                  className="btn btn-md btn-success rounded-5"
-                  style={{}}
-                  onClick={() => {
-                    window.location.href = process.env.REACT_APP_PAYMENT_URL;
-                  }}
-                >
-                  START FREE TRIAL
-                </button>
-              )}
-            </div>
+          <div>
+            <label className="fs-4">
+              Free Trial Remaining{" "}
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  color: "#07874d"
+                }}
+              >
+                {Math.ceil(currentUser.expiredays)}
+              </span>{" "}
+              days
+            </label>
           </div>
         </div>
-
-        <div className="image-stack d-flex justify-content-end align-items-center">
-          <img
-            src={newsImg2}
-            alt=""
-            className="image2"
-            style={{ transform: "rotate(-14deg)", width: "19vh" }}
-          />
-          <img
-            src={newsImg3}
-            alt=""
-            className="image3"
-            style={{ transform: "rotate(13deg)", width: "24vh" }}
-          />
-          <img
-            src={newsImg1}
-            alt=""
-            className="image1"
-            style={{ transform: "rotate(-6deg)", width: "26vh" }}
-          />
-        </div>
-      </div>
-      <div className="main-text-content">
-        <div>Generate your Content</div>
-        <div>
-          <span>in </span>
-          <span className="text-success">3</span>
-          <span> simple steps</span>
-        </div>
-      </div>
-      <div className="main-step">
-        <div>
-          <span id="main-step-number">1</span>
-          <span id="main-step-content">
-            Input the URL <span className="fw-bold">Links </span>to your source
-            articles
-          </span>
-        </div>
-        <div>
-          <span id="main-step-number">2</span>
-          <span id="main-step-content">
-            Choose the type of <span className="fw-bold">Summary </span>and your{" "}
-            <span className="fw-bold">Tone </span>
-            of voice
-          </span>
-        </div>
-        <div>
-          <span id="main-step-number">3</span>
-          <span id="main-step-content">
-            Hit <span className="fw-bold">"Submit"</span> and let our little
-            robots do their thing
-          </span>
-        </div>
-      </div>
-      {currentUser &&
-      (currentUser.subscriptionStatus === "active" ||
-        currentUser.subscriptionStatus === "trialing") ? (
-        <div className="d-flex justify-content-center align-items-center mt-4">
-          <button
-            className="btn btn-md btn-success rounded-5"
-            style={{ width: "20vh" }}
-            onClick={() => {
-              navigate("/mainscreen");
-            }}
+        <div className="d-flex justify-content-center align-items-center">
+          <ToggleButtonGroup
+            className="btn d-flex flex-column flex-wrap justify-content-between align-items-center"
+            type="radio"
+            name="style-options"
+            size="sm"
+            style={{ paddingLeft: "0px" }}
           >
-            CREATE NEWSLETTER
-          </button>
-        </div>
-      ) : (
-        <div className="d-flex justify-content-center align-items-center mt-4">
-          <button
-            className="btn btn-md btn-success rounded-5"
-            style={{ width: "20vh" }}
-            onClick={() => {
-              window.location.href = process.env.REACT_APP_PAYMENT_URL;
-            }}
-          >
-            START FREE TRIAL
-          </button>
-        </div>
-      )}
-
-      <div className="main-foot">
-        <div className="home-bugleai-div" style={{ marginRight: "15%" }}>
-          <div id="main-vector-img">
-            <LogoIcon />
-          </div>
-          <label className="home-bugleai-text">BUGLE AI</label>
-        </div>
-        <div className="main-foot-items">
-          <a className="text-white text-decoration-none" href="/demo">
-            Watch Demo
-          </a>
-          {currentUser && currentUser.subscriptionStatus === "active" ? (
-            <a className="text-white text-decoration-none" href="/mainscreen">
-              Getstarted
-            </a>
-          ) : (
-            <a
-              className="text-white text-decoration-none"
-              href={process.env.REACT_APP_PAYMENT_URL}
+            <ToggleButton
+              variant="outline-light"
+              id="tbg-btn-11"
+              value={30}
+              style={{
+                borderRadius: "21px",
+                width: "25vh",
+                height: "auto",
+                fontSize: "15px",
+                paddingTop: "10px",
+                paddingBottom: "10px"
+              }}
             >
-              START FREE TRIAL
-            </a>
-          )}
-          <a className="text-white text-decoration-none" href="/contactus">
-            ContactUs
-          </a>
+              $19.95 per a month
+            </ToggleButton>
+          </ToggleButtonGroup>
         </div>
-      </div>
-      <div className="footer">
-        <h3 className="home-footer-reserve text-white fs-6 pt-1 pb-5">
-          @Bugle AI 2023, all rights reserved
-        </h3>
+        <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+          <div className="mb-5">
+            <label className="text-white">
+              Payment process:{" "}
+              {currentUser.subscriptionStatus === "active" ||
+              currentUser.subscriptionStatus === "trialing" ? (
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                  <span className="mb-3 mt-1">Active</span>
+                  <botton
+                    className="btn btn-sm btn-outline-light"
+                    onClick={handleOnClick}
+                  >
+                    Cancel
+                  </botton>
+                </div>
+              ) : (
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                  <span className="mb-3">Deactive</span>
+                  <Button
+                    href={process.env.REACT_APP_PAYMENT_URL}
+                    target="_blank"
+                    variant="secondary"
+                    size="md"
+                    style={{ background: "bottom" }}
+                  >
+                    Pay Now
+                  </Button>
+                </div>
+              )}
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
