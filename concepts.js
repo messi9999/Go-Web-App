40eba233d70c73b4c37cb3722dac3dd8f5a4ad81
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-08-14 01:18:31 -0500

    script

--- concepts.js
+++ concepts.js
@@ -1,67 +1,26 @@
 
-import "./App.css";
-import HomeScreen from "./screen/HomeScreen";
-import { Route, Routes, useLocation } from "react-router-dom";
-import MainScreen from "./screen/MainScreen";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import { useDispatch } from "react-redux";
-import { clearMessage } from "./actions/message";
-import { useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ProfileScreen from "./screen/ProfileScreen";
-import AuthVerify from "./common/AuthVerify";
-import { logout } from "./actions/auth";
-import ContactUs from "./screen/ContactUs";
-import VideoPlayer from "./components/VideoPlayer";
-import Adminboard from "./screen/Adminboard";
-import Payment from "./Payment/Payment";
-// import StripePay from "./Payment/StripePay";
-
-function App() {
-  // const [showAdminBoard, setShowAdminBoard] = useState(false);
-
-  // const { user: currentUser } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  let location = useLocation();
-
-  useEffect(() => {
-    if (["/login", "/register"].includes(location.pathname)) {
-      dispatch(clearMessage()); // clear message when changing location
-    }
-  }, [dispatch, location]);
-
-  // useEffect(() => {
-  //   if (currentUser) {
-  //     setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
-  //   } else {
-  //     setShowAdminBoard(false);
-  //   }
-  // }, [currentUser]);
-
-  const logOut = () => {
-    dispatch(logout());
-  };
-
+import React, { useState } from "react";
+import video from "../video/demo.mp4";
+import Header from "./Header";
+const VideoPlayer = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/mainscreen" element={<MainScreen />} />
-        <Route path="/profile" element={<ProfileScreen />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/demo" element={<VideoPlayer />} />
-        <Route path="/admin" element={<Adminboard />} />
-      </Routes>
-      <AuthVerify logOut={logOut} />
+    <div className="home-main bg-black mb-0 bg-gradient py-3">
+      <div style={{ height: "18vh" }}>
+        <Header />
+      </div>
+      <div
+        className="vh-100"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <video width="65%" height="55%" controls>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default VideoPlayer;
 
             
