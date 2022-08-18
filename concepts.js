d758a39513a630d68d03bcd4bd318196755c7b24
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-08-18 01:19:25 -0500

    new

--- concepts.js
+++ concepts.js
@@ -1,35 +1,16 @@
 
-import React from "react";
-import Header from "../components/Header";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-export default function ContactUs() {
-  return (
-    <div>
-      <div style={{ height: "18vh" }}>
-        <div className="home-main bg-black mb-0 bg-gradient py-3">
-          <Header />
-        </div>
-        <div className="bg-black" style={{ height: "110vh" }}>
-          <div className="text-white text-center d-flex flex-column justify-content-center align-content-center w-100 pt-5">
-            <div className="pb-5">
-              <h1>Contact Us</h1>
-            </div>
-            <div>
-              <h4>Email: phil@robotpigeon.co</h4>
-            </div>
-            <div>
-              <label className="fs-4">Bugle AI</label>
-            </div>
-            <div>
-              <label className="fs-4">
-                Colonial House Beverley East Yorkshire UK HU17 0LS
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
 
             
