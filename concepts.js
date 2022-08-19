62eedfbef4e30eb7dd490468d0731bb7b9f113e2
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-08-19 01:19:47 -0500

    new

--- concepts.js
+++ concepts.js
@@ -1,16 +1,26 @@
 
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import React, { useState } from "react";
+import video from "../video/demo.mp4";
+import Header from "./Header";
+const VideoPlayer = () => {
+  return (
+    <div className="home-main bg-black mb-0 bg-gradient py-3">
+      <div style={{ height: "18vh" }}>
+        <Header />
+      </div>
+      <div
+        className="vh-100"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <video width="65%" height="55%" controls>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
 
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-
-export default store;
+export default VideoPlayer;
 
             
