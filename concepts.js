3fcb7772117a289453b2f682b07ecb3d2f756b37
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-09-06 01:25:52 -0500

    functional component

--- concepts.js
+++ concepts.js
@@ -1,174 +1,145 @@
 
-import "./HomeScreen.css";
+import "./UserinfoModal.css";
 import React from "react";
-import Header from "../components/Header";
-
-import newsImg1 from "../img/news1.png";
-import newsImg2 from "../img/news2.png";
-import newsImg3 from "../img/news3.png";
+import { Modal } from "react-bootstrap";
 import { ReactComponent as LogoIcon } from "../img/icon.svg";
+import { ReactComponent as UserAvatar } from "../img/user.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/auth";
 
-export default function HomeScreen() {
+export default function UserinfoModal({ isOpen, onClose }) {
   const navigate = useNavigate();
   const { user: currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(logout());
+  };
+  if (!isOpen) return null;
   return (
-    <div className="home-main bg-black mb-0 bg-gradient py-3">
-      <div style={{ height: "12vh" }}>
-        <Header />
-      </div>
-      <div className="main-body text-white">
-        <div className="main-body-text">
+    <div>
+      <Modal
+        className="modal-user-info"
+        show={isOpen}
+        onHide={onClose}
+        keyboard={false}
+        centered={true}
+        size="md"
+      >
+        <Modal.Header
+          className="bg-dark border-bottom-0"
+          closeButton
+          closeVariant="white"
+        >
+          <Modal.Title>
+            <div className="p-0 d-flex justify-content-start align-items-center">
+              <div>
+                <LogoIcon />
+              </div>
+              <label className="text-white mt-0 ms-3 fs-4">BUGLE AI</label>
+            </div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          className="bg-dark text-white"
+          style={{ outline: "none !important" }}
+        >
           <div>
-            Let's Generate <br />
-          </div>
-          <div>
-            <span className="text-success">Newsletters</span> with AI!
-          </div>
-          <div className="d-flex justify-content-center align-items-center w-100">
-            <div>
-              {currentUser &&
-              (currentUser.subscriptionStatus === "active" ||
-                currentUser.subscriptionStatus === "trialing") ? (
-                <button
-                  className="btn btn-md btn-success rounded-5"
-                  style={{}}
-                  onClick={() => {
-                    navigate("/mainscreen");
-                  }}
-                >
-                  CREATE NEWSLETTER
-                </button>
-              ) : (
-                <button
-                  className="btn btn-md btn-success rounded-5"
-                  style={{}}
-                  onClick={() => {
-                    window.location.href = process.env.REACT_APP_PAYMENT_URL;
-                  }}
-                >
-                  START FREE TRIAL
-                </button>
-              )}
+            {currentUser && (
+              <div>
+                <div className="fs-3 text-center mb-3 text-white">
+                  {currentUser.username}
+                </div>
+              </div>
+            )}
+            <div className="d-flex justify-content-center align-items-center">
+              <div
+                style={{
+                  width: "80px",
+                  height: "80px",
+                  borderRadius: "40px",
+                  backgroundColor: "#198754",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <a href="/profile">
+                  <div id="user-avata">
+                    <UserAvatar
+                      id="user-avata"
+                      style={{
+                        width: "40px",
+                        height: "40px"
+                      }}
+                    />
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="mt-4 btn btn-default fs-6 text-white border-white rounded-5 w-50"
+                onClick={() => {
+                  navigate("/demo");
+                }}
+              >
+                WATCH DEMO
+              </button>
+              <button
+                className="mt-4 btn btn-default fs-6 text-white border-white rounded-5 w-50"
+                onClick={() => {
+                  navigate("/contactus");
+                }}
+              >
+                Contact Us
+              </button>
             </div>
           </div>
-        </div>
-
-        <div className="image-stack d-flex justify-content-end align-items-center">
-          <img
-            src={newsImg2}
-            alt=""
-            className="image2"
-            style={{ transform: "rotate(-14deg)", width: "19vh" }}
-          />
-          <img
-            src={newsImg3}
-            alt=""
-            className="image3"
-            style={{ transform: "rotate(13deg)", width: "24vh" }}
-          />
-          <img
-            src={newsImg1}
-            alt=""
-            className="image1"
-            style={{ transform: "rotate(-6deg)", width: "26vh" }}
-          />
-        </div>
-      </div>
-      <div className="main-text-content">
-        <div>Generate your Content</div>
-        <div>
-          <span>in </span>
-          <span className="text-success">3</span>
-          <span> simple steps</span>
-        </div>
-      </div>
-      <div className="main-step">
-        <div>
-          <span id="main-step-number">1</span>
-          <span id="main-step-content">
-            Input the URL <span className="fw-bold">Links </span>to your source
-            articles
-          </span>
-        </div>
-        <div>
-          <span id="main-step-number">2</span>
-          <span id="main-step-content">
-            Choose the type of <span className="fw-bold">Summary </span>and your{" "}
-            <span className="fw-bold">Tone </span>
-            of voice
-          </span>
-        </div>
-        <div>
-          <span id="main-step-number">3</span>
-          <span id="main-step-content">
-            Hit <span className="fw-bold">"Submit"</span> and let our little
-            robots do their thing
-          </span>
-        </div>
-      </div>
-      {currentUser &&
-      (currentUser.subscriptionStatus === "active" ||
-        currentUser.subscriptionStatus === "trialing") ? (
-        <div className="d-flex justify-content-center align-items-center mt-4">
-          <button
-            className="btn btn-md btn-success rounded-5"
-            style={{ width: "20vh" }}
-            onClick={() => {
-              navigate("/mainscreen");
-            }}
-          >
-            CREATE NEWSLETTER
-          </button>
-        </div>
-      ) : (
-        <div className="d-flex justify-content-center align-items-center mt-4">
-          <button
-            className="btn btn-md btn-success rounded-5"
-            style={{ width: "20vh" }}
-            onClick={() => {
-              window.location.href = process.env.REACT_APP_PAYMENT_URL;
-            }}
-          >
-            START FREE TRIAL
-          </button>
-        </div>
-      )}
-
-      <div className="main-foot">
-        <div className="home-bugleai-div" style={{ marginRight: "15%" }}>
-          <div id="main-vector-img">
-            <LogoIcon />
-          </div>
-          <label className="home-bugleai-text">BUGLE AI</label>
-        </div>
-        <div className="main-foot-items">
-          <a className="text-white text-decoration-none" href="/demo">
-            Watch Demo
-          </a>
-          {currentUser && currentUser.subscriptionStatus === "active" ? (
-            <a className="text-white text-decoration-none" href="/mainscreen">
-              Getstarted
-            </a>
+          {currentUser &&
+          (currentUser.subscriptionStatus === "active" ||
+            currentUser.subscriptionStatus === "trialing") ? (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="btn btn-md btn-success rounded-5"
+                style={{ marginTop: "5vh", marginBottom: "5vh" }}
+                onClick={() => {
+                  navigate("/mainscreen");
+                }}
+              >
+                CREATE NEWSLETTER
+              </button>
+            </div>
           ) : (
-            <a
-              className="text-white text-decoration-none"
-              href={process.env.REACT_APP_PAYMENT_URL}
-            >
-              START FREE TRIAL
-            </a>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="btn btn-md btn-success rounded-5"
+                style={{ marginTop: "5vh", marginBottom: "5vh" }}
+                onClick={() => {
+                  navigate(`${process.env.REACT_APP_PAYMENT_URL}`);
+                }}
+              >
+                START FREE TRIAL
+              </button>
+            </div>
           )}
-          <a className="text-white text-decoration-none" href="/contactus">
-            ContactUs
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <h3 className="home-footer-reserve text-white fs-6 pt-1 pb-5">
-          @Bugle AI 2023, all rights reserved
-        </h3>
-      </div>
+
+          {currentUser && (
+            <div>
+              <div className="fs-5 text-center mb-3 text-white">
+                <a href="/profile">User Info</a>
+                <div>
+                  <a href="/admin">Admin</a>
+                </div>
+                <div className="mt-3" onClick={logOut}>
+                  LogOut
+                </div>
+              </div>
+            </div>
+          )}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
