f1b9435e22d01766d40ba4cc51ad9fc3499b1e06
SWExpertBH0915 <brandonhilbrands915@gmail.com>
2022-09-14 01:27:58 -0500

    new

--- concepts.js
+++ concepts.js
@@ -1,119 +1,145 @@
 
+import "./UserinfoModal.css";
 import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Modal } from "react-bootstrap";
+import { ReactComponent as LogoIcon } from "../img/icon.svg";
+import { ReactComponent as UserAvatar } from "../img/user.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "../components/Header";
-import { Button, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
-import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BASEURL;
-export default function ProfileScreen() {
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/auth";
+
+export default function UserinfoModal({ isOpen, onClose }) {
+  const navigate = useNavigate();
   const { user: currentUser } = useSelector((state) => state.auth);
-
-  const handleOnClick = async () => {
-    axios
-      .put(`${BASE_URL}/payment/cancel/${currentUser.id}`, {
-        SUBSCRIPTION_ID: currentUser.subscriptionId
-      })
-      .then((res) => {
-        localStorage.setItem("user", JSON.stringify(res.data));
-        window.location.reload();
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(logout());
   };
-
-  if (!currentUser) {
-    return <Navigate to="/login" />;
-  }
-
+  if (!isOpen) return null;
   return (
-    <div style={{ height: "18vh" }}>
-      <div className="home-main bg-black mb-0 bg-gradient py-3">
-        <Header />
-      </div>
-      <div className="bg-black" style={{ height: "110vh" }}>
-        <div className="text-white text-center d-flex flex-column justify-content-center align-content-center w-100 pt-5">
-          <div className="pb-5">
-            <h1>{currentUser.username}</h1>
-          </div>
+    <div>
+      <Modal
+        className="modal-user-info"
+        show={isOpen}
+        onHide={onClose}
+        keyboard={false}
+        centered={true}
+        size="md"
+      >
+        <Modal.Header
+          className="bg-dark border-bottom-0"
+          closeButton
+          closeVariant="white"
+        >
+          <Modal.Title>
+            <div className="p-0 d-flex justify-content-start align-items-center">
+              <div>
+                <LogoIcon />
+              </div>
+              <label className="text-white mt-0 ms-3 fs-4">BUGLE AI</label>
+            </div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          className="bg-dark text-white"
+          style={{ outline: "none !important" }}
+        >
           <div>
-            <h4>Email: {currentUser.email}</h4>
-          </div>
-          <div>
-            <label className="fs-4">
-              Free Trial Remaining{" "}
-              <span
+            {currentUser && (
+              <div>
+                <div className="fs-3 text-center mb-3 text-white">
+                  {currentUser.username}
+                </div>
+              </div>
+            )}
+            <div className="d-flex justify-content-center align-items-center">
+              <div
                 style={{
-                  fontWeight: "bolder",
-                  color: "#07874d"
+                  width: "80px",
+                  height: "80px",
+                  borderRadius: "40px",
+                  backgroundColor: "#198754",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center"
                 }}
               >
-                {Math.ceil(currentUser.expiredays)}
-              </span>{" "}
-              days
-            </label>
+                <a href="/profile">
+                  <div id="user-avata">
+                    <UserAvatar
+                      id="user-avata"
+                      style={{
+                        width: "40px",
+                        height: "40px"
+                      }}
+                    />
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="mt-4 btn btn-default fs-6 text-white border-white rounded-5 w-50"
+                onClick={() => {
+                  navigate("/demo");
+                }}
+              >
+                WATCH DEMO
+              </button>
+              <button
+                className="mt-4 btn btn-default fs-6 text-white border-white rounded-5 w-50"
+                onClick={() => {
+                  navigate("/contactus");
+                }}
+              >
+                Contact Us
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="d-flex justify-content-center align-items-center">
-          <ToggleButtonGroup
-            className="btn d-flex flex-column flex-wrap justify-content-between align-items-center"
-            type="radio"
-            name="style-options"
-            size="sm"
-            style={{ paddingLeft: "0px" }}
-          >
-            <ToggleButton
-              variant="outline-light"
-              id="tbg-btn-11"
-              value={30}
-              style={{
-                borderRadius: "21px",
-                width: "25vh",
-                height: "auto",
-                fontSize: "15px",
-                paddingTop: "10px",
-                paddingBottom: "10px"
-              }}
-            >
-              $19.95 per a month
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-        <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-          <div className="mb-5">
-            <label className="text-white">
-              Payment process:{" "}
-              {currentUser.subscriptionStatus === "active" ||
-              currentUser.subscriptionStatus === "trialing" ? (
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <span className="mb-3 mt-1">Active</span>
-                  <botton
-                    className="btn btn-sm btn-outline-light"
-                    onClick={handleOnClick}
-                  >
-                    Cancel
-                  </botton>
+          {currentUser &&
+          (currentUser.subscriptionStatus === "active" ||
+            currentUser.subscriptionStatus === "trialing") ? (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="btn btn-md btn-success rounded-5"
+                style={{ marginTop: "5vh", marginBottom: "5vh" }}
+                onClick={() => {
+                  navigate("/mainscreen");
+                }}
+              >
+                CREATE NEWSLETTER
+              </button>
+            </div>
+          ) : (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <button
+                className="btn btn-md btn-success rounded-5"
+                style={{ marginTop: "5vh", marginBottom: "5vh" }}
+                onClick={() => {
+                  navigate(`${process.env.REACT_APP_PAYMENT_URL}`);
+                }}
+              >
+                START FREE TRIAL
+              </button>
+            </div>
+          )}
+
+          {currentUser && (
+            <div>
+              <div className="fs-5 text-center mb-3 text-white">
+                <a href="/profile">User Info</a>
+                <div>
+                  <a href="/admin">Admin</a>
                 </div>
-              ) : (
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                  <span className="mb-3">Deactive</span>
-                  <Button
-                    href={process.env.REACT_APP_PAYMENT_URL}
-                    target="_blank"
-                    variant="secondary"
-                    size="md"
-                    style={{ background: "bottom" }}
-                  >
-                    Pay Now
-                  </Button>
+                <div className="mt-3" onClick={logOut}>
+                  LogOut
                 </div>
-              )}
-            </label>
-          </div>
-        </div>
-      </div>
+              </div>
+            </div>
+          )}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
